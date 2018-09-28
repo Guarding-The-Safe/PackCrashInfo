2d34e4f16bf2ae8ae69587e2326ad483406d2f8e
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2018-09-28 19:22:41 +0200

    Now also logs into the normal Log

--- src/main/java/atm/bloodworkxgaming/packcrashinfo/PackCrashInfo.java
+++ src/main/java/atm/bloodworkxgaming/packcrashinfo/PackCrashInfo.java
@@ -3,9 +3,9 @@ package atm.bloodworkxgaming.packcrashinfo;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 
 import static atm.bloodworkxgaming.packcrashinfo.PackCrashInfo.VERSION;
 
@@ -14,9 +14,19 @@ public class PackCrashInfo {
     public static final String MOD_ID = "packcrashinfo";
     public static final String VERSION = "%VERSION%";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    private static final PackCrashCallable callable = new PackCrashCallable();
 
     @Mod.EventHandler
     public void construct(FMLConstructionEvent event) {
-        FMLCommonHandler.instance().registerCrashCallable(new PackCrashCallable());
+        FMLCommonHandler.instance().registerCrashCallable(callable);
+
+        String information = callable.call();
+        String header = "|" + StringUtils.repeat('-', information.length() + 2) + "|";
+        String name = "Modpack Information";
+        LOGGER.info(header);
+        LOGGER.info("| " + name + StringUtils.repeat(' ', information.length() - name.length()) + " |");
+        LOGGER.info("| " + information + " |");
+        LOGGER.info(header);
+
     }
 }
